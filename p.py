fa88168a36c3c9846a541427feb7bce20638eee4
patrickk123 <pratikthenautiyal@gmail.com>
2020-03-26 23:13:57 +0530

    with exe3 and plt

--- p.py
+++ p.py
@@ -1,14 +1,14 @@
-#import matplotlib as mp
-#import pandas as pd
-#from matplotlib import pyplot as plt
+import matplotlib as mp
+import pandas as pd
+from matplotlib import pyplot as plt
 
-#df = pd.read_csv("C:\\Users\\Prateek Nautiyal\\Downloads\\Book1.csv")
-#df.plot()
-#x = [1,2,3,4]
-#y = [1,2,3,4]
-#plt.plot(x,y,"+")
-#plt.title("graph X VS Y")
-#plt.xlabel("X")
-#plt.ylabel("Y")
-#plt.show()
+df = pd.read_csv("C:\\Users\\Prateek Nautiyal\\Downloads\\Book1.csv")
+df.plot()
+x = [1,2,3,4]
+y = [1,2,3,4]
+plt.plot(x,y,"+")
+plt.title("graph X VS Y")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
 print("hello43")
