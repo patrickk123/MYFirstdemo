6df08713a8d3be3fdefcaf7ef7ab0afa69a97f44
patrickk123 <pratikthenautiyal@gmail.com>
2020-03-26 23:23:07 +0530

    with exe3 and plt

--- p.py
+++ p.py
@@ -2,8 +2,8 @@ import matplotlib as mp
 import pandas as pd
 from matplotlib import pyplot as plt
 
-df = pd.read_csv("C:\\Users\\Prateek Nautiyal\\Downloads\\Book1.csv")
-df.plot()
+#df = pd.read_csv("C:\\Users\\Prateek Nautiyal\\Downloads\\Book1.csv")
+#df.plot()
 x = [1,2,3,4]
 y = [1,2,3,4]
 plt.plot(x,y,"+")
